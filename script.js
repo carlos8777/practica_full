b9e33407031c548faf1c533f19e34659bf7b14c7
Carlos <sa475677@uaeh.edu.mx>
2026-08-27 16:11:21 -0600

    Primer intento

---
+++ script.js
@@ -0,0 +1,52 @@
+const formulario = document.getElementById('formulario');
+const resultado = document.getElementById('tarjeta_resultado');
+
+async function obtenerUsuarios() {
+    try{
+        const respuesta = await fetch('http://localhost:3000/api/usuarios');
+        const datos = await respuesta.json();
+
+        if (datos.exito) {
+            console.log('Lista de usuarios de la base: ', datos.usuarios);
+        }
+    } catch (error) {
+        console.error('Error al obtener usuarios: ', error);
+    }
+}
+
+formulario.addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+    const nombre = document.getElementById('nombre').value;
+    const correo = document.getElementById('correo').value;
+    const telefonoInput = document.getElementById('telefono').value;
+    const fecha_nacimiento = document.getElementById('fecha_nacimiento').value;
+    const telefono = telefonoInput ? parseInt(telefonoInput, 10) : null;
+
+    try {
+        const respuesta = await fetch('http://localhost:3000/api/guardar', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ nombre, correo, telefono, fecha_nacimiento })
+        });
+
+        const datos = await respuesta.json();
+
+        if (respuesta.ok) {
+            resultado.innerHTML = `
+                <h3 style="color: #28a745;">${datos.mensaje}</h3>
+                <p><strong>ID asignado por BD:</strong> ${datos.datosRecibidos.id}</p>
+                <p><strong>Nombre:</strong> ${datos.datosRecibidos.nombre}</p>
+                <p><strong>Correo:</strong> ${datos.datosRecibidos.correo}</p>
+            `;
+            formulario.reset();
+            // Volvemos a consultar para verificar que se guardó
+            obtenerUsuarios();
+        } else {
+            resultado.innerHTML = `<p style="color: red;">Error BD: ${datos.error}</p>`;
+        }
+    } catch (error) {
+        resultado.innerHTML = `<p style="color: red;">Error de conexión con el servidor.</p>`;
+    }
+});
+obtenerUsuarios();
---
+++ server.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+console.log('URL de Supabase:', process.env.SUPABASE_URL)
+const express = require('express');
+const cors = require('cors');
+const { createClient } = require('@supabase/supabase-js');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+app.post('/api/guardar', async (req, res) => {
+    const { nombre, correo, telefono, fecha_nacimiento } = req.body;
+
+    const  { data, error } = await supabase
+    .from('usuarios')
+    .insert([{ nombre, correo, telefono, fecha_nacimiento }])
+    .select();
+
+    if(error) {
+        console.error('Error al insertar en Supabase', error.message);
+        return res.status(500).json({ exito: false, error: error.message });
+    }
+
+    console.log('Registro guardado en Supabase:', data);
+    res.json({
+        mensaje: 'Regristro guardado con exito',
+        datosRecibidos: data[0]
+    });
+});
+
+app.get('/api/usuarios', async (req, res) =>{
+    const {data,error} = await supabase
+    .from('usuarios')
+    .select('*');
+
+    if(error){
+        console.error('Error al consultar a Supabase: ', error.message);
+        return res.status(500).json({ exito: false, error: error.message});
+    }
+
+    res.json({
+        exito: true,
+        usuarios: data
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+})
